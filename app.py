b7ed0de957e561dd69c49cad1983a307c9c1667e
Lucas Pereira <lucassousaper7@gmail.com>
2024-02-07 13:33:52 -0300

    6º atualização

--- app.py
+++ app.py
@@ -1,19 +1,60 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
-from registro import registro_usuario_blueprint
+import mysql.connector
+import bcrypt
 
 app = Flask(__name__)
 CORS(app)
 
+conn = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '2706Lps@',
+    'database': 'LOGIN'
+}
+
+# Função para conectar ao banco de dados
+def conectar_banco():
+    try:
+        # Conectar ao banco de dados
+        conexao = mysql.connector.connect(
+            host=conn['host'],
+            user=conn['user'],
+            password=conn['password'],
+            database=conn['database'])
+        print("Conexão bem-sucedida ao banco de dados!")
+        return conexao
+    except mysql.connector.Error as erro:
+        print(f"Erro ao conectar ao banco de dados: {erro}")
+        return None
+
+# Rota para renderizar o formulário de registro
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/static/<path:path>')
-def send_static(path):
-    return send_from_directory('static', path)
+# Rota para lidar com o registro de usuários
+@app.route('/registrar', methods=['POST'])
+def registro_usuario():
+    data = request.form
+    nome = data['nome']
+    senha = data['senha']
+    confirm_password = data['confirm_password']
 
-app.register_blueprint(registro_usuario_blueprint)
+    if senha == confirm_password:
+        conexao = conectar_banco()
+        if conexao is not None:
+            try:
+                hashed_password = bcrypt.hashpw(senha.encode('utf-8'), bcrypt.gensalt())
+                cursor = conexao.cursor()
+                cursor.execute("INSERT INTO usuarios (nome, senha) VALUES (%s, %s)", (nome, hashed_password))
+                conexao.commit()
+                conexao.close()
+                return jsonify({'message': 'Usuário registrado com sucesso!'}), 201
+            except mysql.connector.Error as erro:
+                return jsonify({'error': f"Erro ao registrar usuário: {erro}"}), 500
+    else:
+        return jsonify({'error': 'As senhas não coincidem.'}), 400
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000, debug=True)
